9db55e09e4ec3373e3fcaaa0178bd17533c1bba2
jstrangfeld <jesse.strangfeld@gmail.com>
2014-12-04 16:41:32 -0500

    Create app.js

---
+++ js/app.js
@@ -0,0 +1,18 @@
+$(function () {
+  var map = L.map('map', {
+    layers: [
+      L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
+      	attribution: '&copy; <a href="http://cartodb.com/attributions">CartoDB</a>',
+      	subdomains: 'abcd',
+      	minZoom: 0,
+      	maxZoom: 18
+      }),
+      L.tileLayer('http://openmapsurfer.uni-hd.de/tiles/adminb/x={x}&y={y}&z={z}', {
+      	minZoom: 0,
+      	maxZoom: 19,
+      	attribution: 'Imagery from <a href="http://giscience.uni-hd.de/">GIScience Research Group @ University of Heidelberg</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+      })
+    ]
+  })
+})
+    
