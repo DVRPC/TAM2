ae42c32cdb3596a631def56d5864cd70f680bef7
Jesse Strangfeld <jesse.strangfeld@gmail.com>
2015-02-10 14:03:10 -0500

    Fix miscalculated polygon area

--- js/app.js
+++ js/app.js
@@ -43,20 +43,6 @@ function updateDownloadURL(roadSegments) {
   $('.btn-create').attr('href', 'data:text/csv;charset=utf-8,' + encodeURIComponent(csvArray.join('\n')))
 }
 
-function polygonArea(points) {
-  var sum = 0.0;
-  var length = points.length;
-  if (length < 3) {
-    return sum;
-  }
-  points.forEach(function(d1, i1) {
-    i2 = (i1 + 1) % length;
-    d2 = points[i2];
-    sum += (d2[1] * d1[0]) - (d1[1] * d2[0]);
-  });
-  return sum / 2;
-}
-
 function getDistance(array, decimals) {
   if (Number.prototype.toRad === undefined) {
     Number.prototype.toRad = function () {
@@ -144,8 +130,8 @@ $(function () {
       .on(btn, 'click', function () {
         var modal = $('#modal-repair'),
         area = Math.abs(editable.getLayers().filter(function (l) { return l.toGeoJSON().geometry.type === 'Polygon' }).reduce(function (prev, cur) {
-          return prev + polygonArea(cur.toGeoJSON().geometry.coordinates[0])
-        }, 0) * 1195990.04994),
+          return prev + L.GeometryUtil.geodesicArea(cur.getLatLngs())
+        }, 0) * 1195990.046296),
         len = editable.getLayers().filter(function (l) { return l.toGeoJSON().geometry.type === 'LineString' }).reduce(function (prev, cur) {
           return prev + getDistance(cur.toGeoJSON().geometry.coordinates)
         }, 0) * 0.62137119
